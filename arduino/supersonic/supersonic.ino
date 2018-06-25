c6d62c3daa07b09dc4eb7e98278aeb403c526fa7
sollimann <kristrso@stud.ntnu.no>
2018-06-25 16:55:49 +0200

    Removed some unneccesary code

--- arduino/supersonic/supersonic.ino
+++ arduino/supersonic/supersonic.ino
@@ -7,8 +7,11 @@
 #define TRIGGER_PIN3  13
 #define ECHO_PIN3     13
 #define MAX_DISTANCE 200
+
+// Define number of devices connected
 #define NUM_PINGS 3
 
+// Add pins of each respective device
 NewPing sonar[NUM_PINGS] = {
   NewPing(TRIGGER_PIN1, ECHO_PIN1, MAX_DISTANCE),
   NewPing(TRIGGER_PIN2, ECHO_PIN2, MAX_DISTANCE),
@@ -16,19 +19,15 @@ NewPing sonar[NUM_PINGS] = {
 };
 
 
-//NewPing sonar(TRIGGER_PIN2, ECHO_PIN2, MAX_DISTANCE);
- 
+// Choose serial connection 
 void setup() {
   Serial.begin(115200);
 }
+
+
  
 void loop() {
-  /*
-  delay(50);
-  Serial.print("Ping: ");
-  Serial.print(sonar.ping_cm());
-  Serial.println(" cm");
-*/
+  
     unsigned int dists[NUM_PINGS];
   for (int i=0; i<NUM_PINGS; i++){
     delay(25);
